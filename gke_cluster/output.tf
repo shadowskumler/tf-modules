5a4ebe0ebde33ca5332d2d781cca3ca3dca7ff5a
Anders Bruun Olsen <anders.olsen@cellpointmobile.com>
2019-01-17 19:33:29 +0100

    Add tiller service account output to gke_cluster module.

--- gke_cluster/output.tf
+++ gke_cluster/output.tf
@@ -21,3 +21,7 @@ output "client_key" {
 output "cluster_ca_certificate" {
   value = "${google_container_cluster.cluster.master_auth.0.cluster_ca_certificate}"
 }
+
+output "tiller-service-account" {
+  value = "${kubernetes_service_account.tiller.metadata.0.name}"
+}
