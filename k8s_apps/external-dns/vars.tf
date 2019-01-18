f6c232b92ec48a456a5f5169e357773e47364ec0
Anders Bruun Olsen <anders.olsen@cellpointmobile.com>
2019-01-18 11:04:27 +0100

    Add default for repository and add version var for external-dns module.

--- k8s_apps/external-dns/vars.tf
+++ k8s_apps/external-dns/vars.tf
@@ -1,8 +1,14 @@
 variable "repository" {
-  description = "Helm repository that contains external-dns chart."
+  description = "Helm repository that contains external-dns chart. Defaults to stable kubernetes charts repo."
+  default     = "https://kubernetes-charts.storage.googleapis.com"
 }
 
 variable "namespace" {
-  description = "Namespace to install external-dns in."
+  description = "Namespace to install external-dns in. Defaults to 'ingress'."
   default     = "ingress"
+}
+
+variable "version" {
+  description = "Version of chart to deploy."
+  default     = ""
 }
